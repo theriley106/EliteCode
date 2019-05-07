4d20d21f97370d53ec62f7745953e35c6b319d89
theriley106 <christopherlambert106@gmail.com>
2019-05-07 10:15:42 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='elitecode',
+      version='1.0',
+      description='Strategically Shuffling Leetcode Questions to Optimize DS&A Study Time',
+      author='Christopher Lambert',
+      author_email='lambertcr@outlook.com',
+      url='https://github.com/theriley106/EliteCode',
+      license='MIT',
+      packages=['elitecode'],
+      install_requires=['requests']
+     )
