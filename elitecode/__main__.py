762f9e7a39dc431b7aa7ab6c5e167ba4bbc0116d
theriley106 <christopherlambert106@gmail.com>
2019-05-07 10:43:56 -0400

    Added working main

--- elitecode/__main__.py
+++ elitecode/__main__.py
@@ -3,23 +3,25 @@ import argparse
 
 DEFAULTS = {'medium': 30, 'hard': 10, 'easy': 60}
 
-def make_args_100(args):
-	countVal = sum(args.values())
-	if countVal != 100:
-	for k, v in args.iteritems():
-
 
 
 if __name__ == '__main__':
-
 	parser = argparse.ArgumentParser(description='Description of your program')
-	parser.add_argument('--easy', help='Signifies the percentage of questions classified as "Easy" in the problem pool', required=False, default=DEFAULTS['easy'])
-	parser.add_argument('--medium', help='Signifies the percentage of questions classified as "Medium" in the problem pool', required=False, default=DEFAULTS['medium'])
-	parser.add_argument('--hard', help='Signifies the percentage of questions classified as "Hard" in the problem pool', required=False, default=DEFAULTS['hard'])
-	args = vars(parser.parse_args())
-	for key, val in args.iteritems():
-		try:
-			float(val)
-		except:
-			raise Exception("Numerical values required for command line arguments")
+	parser.add_argument('--easy', help='Signifies the ratio of questions classified as "Easy" in the problem pool', required=False)
+	parser.add_argument('--medium', help='Signifies the ratio of questions classified as "Medium" in the problem pool', required=False)
+	parser.add_argument('--hard', help='Signifies the ratio of questions classified as "Hard" in the problem pool', required=False)
+	args = {k: v for k, v in vars(parser.parse_args()).iteritems() if v is not None}
 	print args
+	if len(args) > 0:
+		if len(args) != 3:
+			raise Exception("Please specify ratios for all 3 types of Leetcode question (--easy, --medium, --hard)")
+		for key, val in args.iteritems():
+			try:
+				DEFAULTS[key] = float(val)
+			except:
+				raise Exception("Numerical values required for command line arguments")
+	easy = DEFAULTS['easy']
+	medium = DEFAULTS['medium']
+	hard = DEFAULTS['hard']
+	# Specifies default args
+	print("Shuffling Leetcode questions with ratio: E: {} M: {} H: {}".format(easy, medium, hard))
